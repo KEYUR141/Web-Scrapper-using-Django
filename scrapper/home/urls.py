74ef4bf879a446794b092d7e3cf5fb987ea1c7e4
Keyur Sherke <168756352+KEYUR141@users.noreply.github.com>
2025-04-20 18:06:34 +0530

    Scrapper

---
+++ scrapper/home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from home.views import *
+
+urlpatterns = [
+    path('imdb_scrap',imdb_index,name="IMDBHome"),
+    path('imdb_run_scrapper/',imdb_run_scrapper),
+    path('toi_run_scrapper/',toi_run_scrapper),
+    path('toi_scrap',toi_index,name="TOIHome"),
+    path('',Main_Page)
+
+]
