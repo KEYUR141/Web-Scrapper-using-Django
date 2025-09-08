3c64b79a8674f2d7edcc1d6c0573e8cd63551309
KEYUR141 <bravao141.kyr@gmail.com>
2025-09-08 14:17:51 +0530

    Imroved the UI

--- scrapper/home/views.py
+++ scrapper/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse,HttpResponse
 from scrapper_script import *
 from django.db.models import Q
@@ -15,23 +15,20 @@ def imdb_run_scrapper(request):
 
 def imdb_index(request):
     data = IMBD_News.objects.all()
+    # Handle navbar search redirect
+    navbar_search = request.GET.get('navbar_search')
+    if navbar_search:
+        return redirect(f"/imdb_scrap?imdb_search={navbar_search}")
 
     search = request.GET.get('imdb_search')
-    
     if search:
-        
-        
         data = data.filter(
             Q(title__icontains = search) |
             Q(description__icontains = search)
         )
-        
- 
-    
     return render(request,"IMDBindex.html",context={
-       
         'data':data,
-        'imdb_search':search
+        'imdb_search':search or request.GET.get('navbar_search')
     })
     
 
@@ -46,22 +43,20 @@ def toi_run_scrapper(request):
 def toi_index(request):
 
     data = TOI_News.objects.all()
+    # Handle navbar search redirect
+    navbar_search = request.GET.get('navbar_search')
+    if navbar_search:
+        return redirect(f"/toi_scrap?toi_search={navbar_search}")
 
     search = request.GET.get('toi_search')
-    
     if search:
-        
-        
         data = data.filter(
             Q(title__icontains = search) |
             Q(description__icontains = search)
         )
-        
- 
-    
     return render(request,"TOIindex.html",context={
         'data':data,
-        'toi_search':search
+        'toi_search':search or request.GET.get('navbar_search')
     })
 
 def Main_Page(request):
